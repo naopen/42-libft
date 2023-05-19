ae2198fbe1b492a6ae6b4c47f0aa41a9c0f1dcfd
naopen <naokizssm@gmail.com>
2023-05-19 17:48:14 +0900

    Add: libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 08:45:48 by nkannan           #+#    #+#             */
-/*   Updated: 2023/05/18 08:47:30 by nkannan          ###   ########.fr       */
+/*   Updated: 2023/05/19 17:11:48 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,26 @@
 
 # include <stddef.h>
 
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int ch);
+int		ft_isprint(int ch);
+size_t	ft_strlen(const char *str);
 void	*ft_memset(void *buf, int ch, size_t n);
 void	ft_bzero(void *buf, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memccpy(void *buf1, const void *buf2, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst,const char *src, size_t dstsize);
+int		ft_toupper(int ch);
+int		ft_tolower(int ch);
+char	*ft_strchr(const char *str, int ch);
+char	*ft_strrchr(const char *str, int ch);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *buf, int ch, size_t n);
 int		ft_memcmp(const void *buf1, const void *buf2, size_t n);
-size_t	ft_strlen(const char *str);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *str);
 
 #endif
