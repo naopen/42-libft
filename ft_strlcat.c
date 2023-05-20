ef1689694174388cbeb0768ada02d35bba33ad11
naopen <naokizssm@gmail.com>
2023-05-20 19:47:57 +0900

    Update: ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 09:56:31 by nkannan           #+#    #+#             */
-/*   Updated: 2023/05/20 14:14:09 by nkannan          ###   ########.fr       */
+/*   Updated: 2023/05/20 19:47:01 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@
 // dstsize-strlen(*dst) - 1 バイトコピーする前に
 // ヌル文字 ('\0') が見つかると、そこでコピーを中止する。
 
-// dstsizeのサイズが0の場合はsrcの長さを返す。
-// この際にstrlenを使うと、crashしてしまう。
+// 返り値は、コピーしようとした文字列の長さを返す。
+// つまり、dstsizeが0であっても、srcの長さを返す。
+// またこの際にstrlenを使うと、crashしてしまう。
 // これは、strlenは、ヌル文字が見つかるまでの長さを返すためである。
 // 一方で、strnlenは、dstsizeのサイズ、つまりここでは0文字分の長さを返すため、
 // ヌル文字が見つからない場合にメモリ領域を破壊してしまうことを防ぐことができる
@@ -40,6 +41,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	dst_len = ft_strnlen(dst, dstsize);
 	src_len = ft_strlen(src);
+	if (dstsize == 0)
+		return (src_len);
 	if (!dst && dstsize == 0)
 		return (ft_strnlen(src, dstsize));
 	if (dstsize <= dst_len)
