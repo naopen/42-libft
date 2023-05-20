38c610292aaccea1ba2d2abf1113eea2c7b43dc2
naopen <naokizssm@gmail.com>
2023-05-20 23:11:23 +0900

    Update: ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 08:38:20 by nkannan           #+#    #+#             */
-/*   Updated: 2023/05/20 08:47:34 by nkannan          ###   ########.fr       */
+/*   Updated: 2023/05/20 22:34:43 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,32 @@
 
 static int	ft_isspace(char c)
 {
-	if (c == '\t' || c == '\n' || c == '\v' || \
-		c == '\f' || c == '\r' || c == ' ')
+	if (c == '\t' || c == '\n' || c == '\v'
+		|| c == '\f' || c == '\r' || c == ' ')
 		return (1);
 	return (0);
 }
 
-int	ft_atoi(const char *str)
+// numがオーバーフローするかどうかを判定する関数
+static int	ft_is_overflow(unsigned long long num, int sign, unsigned int digit)
 {
-	int			sign;
-	long long	num;
+	if (num > LONG_MAX / 10 || (num == LONG_MAX / 10 && digit > LONG_MAX % 10))
+	{
+		if (sign == 1)
+			return (1);
+		else
+			return (-1);
+	}
+	return (0);
+}
 
-	sign = 1;
+static long	ft_strtol(const char *restrict str, char **restrict endptr)
+{
+	unsigned long long	num;
+	int					sign;
+
 	num = 0;
+	sign = 1;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '-')
@@ -41,10 +54,22 @@ int	ft_atoi(const char *str)
 	if (*str == '-' || *str == '+')
 		str++;
 	while (ft_isdigit(*str))
+	{
+		if (ft_is_overflow(num, sign, *str - '0'))
+		{
+			if (sign == 1)
+				return (LONG_MAX);
+			else
+				return (LONG_MIN);
+		}
 		num = num * 10 + (*str++ - '0');
-	if (num > 2147483647 && sign == 1)
-		return (-1);
-	if (num > 2147483648 && sign == -1)
-		return (0);
+	}
+	if (endptr)
+		*endptr = (char *)str;
 	return (num * sign);
+}
+
+int	ft_atoi(const char *str)
+{
+	return ((int)ft_strtol(str, NULL));
 }
