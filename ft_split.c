62301218b973853376ec1ee9a027feb9463417bb
naopen <naokizssm@gmail.com>
2023-05-21 15:22:01 +0900

    Update: ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 14:31:10 by nkannan           #+#    #+#             */
-/*   Updated: 2023/05/21 15:08:37 by nkannan          ###   ########.fr       */
+/*   Updated: 2023/05/21 15:20:24 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,33 +60,42 @@ static char	**ft_free(char **split, size_t i)
 	return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**ft_split_helper(char const *str, char ch, char **split,
+		size_t count)
 {
-	char	**split;
 	size_t	i;
 	size_t	j;
 	size_t	k;
 
-	if (!s)
-		return (NULL);
-	split = (char **)malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
-	if (!split)
-		return (NULL);
 	i = 0;
 	j = 0;
-	while (i < ft_count_words(s, c))
+	while (i < count)
 	{
-		k = 0;
-		while (s[j] == c)
+		while (str[j] == ch)
 			j++;
-		split[i] = (char *)malloc(sizeof(char) * (ft_word_len(s, c, j) + 1));
+		split[i] = (char *)malloc(sizeof(char) * (ft_word_len(str, ch, j) + 1));
 		if (!split[i])
 			return (ft_free(split, i));
-		while (s[j] != c && s[j])
-			split[i][k++] = s[j++];
+		k = 0;
+		while (str[j] != ch && str[j])
+			split[i][k++] = str[j++];
 		split[i][k] = '\0';
 		i++;
 	}
 	split[i] = NULL;
 	return (split);
+}
+
+char	**ft_split(char const *str, char ch)
+{
+	char	**split;
+	size_t	count;
+
+	if (!str)
+		return (NULL);
+	count = ft_count_words(str, ch);
+	split = (char **)malloc(sizeof(char *) * (count + 1));
+	if (!split)
+		return (NULL);
+	return (ft_split_helper(str, ch, split, count));
 }
